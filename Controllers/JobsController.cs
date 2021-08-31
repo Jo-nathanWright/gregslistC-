cb3e88fd4e1bab5539ccaf7bd1b70778fb2591a0
Jonathan Wright <j.wright72127@gmail.com>
2021-08-31 16:21:55 -0600

    Finished Jobs

--- Controllers/JobsController.cs
+++ Controllers/JobsController.cs
@@ -1,7 +1,82 @@
+using System;
+using System.Collections.Generic;
+using gregslist.Models;
+using gregslist.Service;
+using Microsoft.AspNetCore.Mvc;
+
 namespace gregslist.Controllers
 {
-    public class JobsController
+
+    [ApiController]
+
+    [Route("/api/[controller]")]
+    public class JobsController : ControllerBase
     {
-        
+        private readonly JobsService _jobService;
+
+        public JobsController(JobsService jobsService)
+        {
+          _jobService = jobsService;
+        }
+
+        [HttpGet]
+
+        public ActionResult<IEnumerable<Job>> Get()
+        {
+            try
+            {
+              IEnumerable<Job> jobs = _jobService.Get();
+              return Ok(jobs);
+            }
+            catch (Exception err)
+            {
+              return BadRequest(err.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<Job> Get(string id)
+        {
+            try
+            {
+              Job found = _jobService.Get(id);
+              return Ok(found);
+            }
+            catch (Exception err)
+            {
+              return BadRequest(err.Message);
+            }
+        }
+
+        [HttpPost]
+
+        public ActionResult<Job> Create([FromBody] Job rawJob)
+        {
+            try
+            {
+              Job job = _jobService.Create(rawJob);
+              return Ok(job);
+            }
+            catch (Exception err)
+            {
+              return BadRequest(err.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+
+        public ActionResult<Job> Delete(string id)
+        {
+            try
+            {
+              _jobService.Delete(id);
+              return Ok("Successfully Deleted That Job Listing");
+            }
+            catch (Exception err)
+            {
+              return BadRequest(err.Message);
+            }
+        }
     }
 }
--- Service/JobsService.cs
+++ Service/JobsService.cs
@@ -1,7 +1,37 @@
+using System;
+using System.Collections.Generic;
+using gregslist.Models;
+
 namespace gregslist.Service
 {
     public class JobsService
     {
-        
+        internal IEnumerable<Job> Get()
+        {
+          return FakeDB.Jobs;
+        }
+
+        internal Job Get(string id)
+        {
+          Job found = FakeDB.Jobs.Find(j => j.Id == id);
+          if(found == null){
+            throw new Exception("Invalid Id");
+          }
+          return found;
+        }
+
+        internal Job Create(Job newJob)
+        {
+          FakeDB.Jobs.Add(newJob);
+          return newJob;
+        }
+
+        internal void Delete(string id)
+        {
+          int deleted = FakeDB.Jobs.RemoveAll(j => j.Id == id);
+          if(deleted == 0) {
+            throw new Exception("Invalid Id");
+          }
+        }
     }
 }
