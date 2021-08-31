7add5bc5ec9a5cd55d3f660dfc776bb8e4d9eb95
Jonathan Wright <j.wright72127@gmail.com>
2021-08-31 16:07:53 -0600

    Set up Job Model and FakeDb

--- Models/Job.cs
+++ Models/Job.cs
@@ -1,7 +1,36 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace gregslist.Models
 {
     public class Job
     {
-        
+        public string Id { get; set; }
+        [Required]
+
+        public string Company { get; set; }
+        [Required]
+
+        public string JobTitle { get; set; }
+        [Required]
+        [Range(0, int.MaxValue)]
+
+        public int Hours { get; set; }
+        [Required]
+        [Range(0, int.MaxValue)]
+
+        public int Rate { get; set; }
+
+        public string Description { get; set; }
+
+        public Job(string company, string jobTitle, int hours, int rate, string description = "None Provided")
+        {
+          Company = company;
+          JobTitle = jobTitle;
+          Hours = hours;
+          Rate = rate;
+          Description = description;
+          Id = Guid.NewGuid().ToString();
+        }
     }
 }
