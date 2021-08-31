424c554d0d798807a4a343d0ace7c6f8eabcf15f
Jonathan Wright <j.wright72127@gmail.com>
2021-08-31 15:09:42 -0600

    Made House Model and FakeDB

--- FakeDB.cs
+++ FakeDB.cs
@@ -6,5 +6,7 @@ namespace gregslist
     static public class FakeDB
     {
     static public List<Car> Cars { get; set; } = new List<Car>() { new Car("Mystery", "Machine", 1974, 900000, "Filled With a Strange Snack...", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/cdabf980-8171-402b-9974-257fff37b0e6/ddym1vj-7716d714-4a40-4b6f-9d61-8bd384f03297.png/v1/fill/w_920,h_520,strp/cyber_mystery_machine_by_jjmunden_ddym1vj-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTIwIiwicGF0aCI6IlwvZlwvY2RhYmY5ODAtODE3MS00MDJiLTk5NzQtMjU3ZmZmMzdiMGU2XC9kZHltMXZqLTc3MTZkNzE0LTRhNDAtNGI2Zi05ZDYxLThiZDM4NGYwMzI5Ny5wbmciLCJ3aWR0aCI6Ijw9OTIwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.O6wY9aQ7edWvS5LYiNgKauE0BwBuUZUewujQ2akMRHQ") };
+
+    static public List<House> Houses { get; set; } = new List<House>() { new House(5, 2, 2008, 1, 500000) };
   }
 }
--- Models/House.cs
+++ Models/House.cs
@@ -1,7 +1,45 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace gregslist.Models
 {
     public class House
     {
-        
+        public string Id { get; set; }
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Bedrooms { get; set; }
+        [Required]
+        [Range(0, int.MaxValue)]
+
+        public int Bathrooms { get; set; }
+        [Required]
+        [Range(1800, 2021)]
+
+        public int Year { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
+
+        public int Levels { get; set; }
+        [Required]
+        [Range(0, int.MaxValue)]
+
+        public int Price { get; set; }
+
+    public string Description { get; set; }
+
+        public string ImgUrl { get; set; }
+
+    public House(int bedrooms, int bathrooms, int year, int levels, int price, string description = "Not Provided", string imgUrl = "https://via.placeholder.com/420/0000FF/808080")
+    {
+      Bedrooms = bedrooms;
+      Bathrooms = bathrooms;
+      Year = year;
+      Levels = levels;
+      Price = price;
+      Description = description;
+      ImgUrl = imgUrl;
+      Id = Guid.NewGuid().ToString();
     }
+  }
 }
